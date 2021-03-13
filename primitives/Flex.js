651a27d4eeb59f22a56e54b542e5c3d42ce3098f
Jose Cuevas <josecueram@gmail.com>
2021-03-13 13:27:24 -0600

    Create Flex Primitive

---
+++ primitives/Flex.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Box } from '@/primitives/Box'
+import { flexbox } from 'styled-system'
+
+export const Flex = styled(Box)`
+  display: flex;
+  ${flexbox}
+`
