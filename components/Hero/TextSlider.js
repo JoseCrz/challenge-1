69c0fdfd1aefb90ce28208baa94dc5bbcc864be9
Jose Cuevas <josecueram@gmail.com>
2021-03-13 13:29:38 -0600

    Add Basic Dotslider to TextSlider

--- components/Hero/TextSlider.js
+++ components/Hero/TextSlider.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import { Button } from '@/components/Button'
 import { Text } from '@/primitives/Text'
 import { Box } from '@/primitives/Box'
+import { Flex } from '@/primitives/Flex'
+import { GridLayout, GridItem } from '@/primitives/Grid'
 
 const HighlightedText = styled.span`
   display: inline-block;
@@ -31,13 +33,47 @@ const Highlight = ({ children }) => (
   </Box>
 )
 
+const Dot = styled.div`
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.gray};
+  margin-top: ${({ theme }) => theme.space[2]};
+`
+
+const DotSlider = () => {
+  return (
+    <Box>
+      <Dot />
+      <Dot />
+      <Dot />
+      <Dot />
+      <Dot />
+    </Box>
+  )
+}
+
 export const TextSlider = () => {
   return (
     <>
-      <Text as='h1' fontSize={6}>
-        Start <Highlight>energy</Highlight> revolution from your community
-      </Text>
-      <Button>Contact us</Button>
+      <GridLayout gridTemplateColumns='100px 1fr'>
+        <GridItem>
+          <Flex
+            height='100%'
+            flexDirection='column'
+            justifyContent='flex-end'
+            alignItems='center'
+          >
+            <DotSlider />
+          </Flex>
+        </GridItem>
+        <GridItem>
+          <Text as='h1' fontSize={6}>
+            Start <Highlight>energy</Highlight> revolution from your community
+          </Text>
+          <Button>Contact us</Button>
+        </GridItem>
+      </GridLayout>
     </>
   )
 }
